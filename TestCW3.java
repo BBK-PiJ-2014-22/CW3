3394bb92f345a180b56da4e62b8b616bda6b00bc
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2014-12-20 20:13:50 +0000

    Test 90% written
    Need to add specific messages for individual fails and then test the
    tests.

--- TestCW3.java
+++ TestCW3.java
@@ -13,7 +13,49 @@ public class TestCW3 {
 	}
 
 	
-	public void testEnum(){
+	public void testReturnObjectImpl(Object object, ErrorMessage error, String message){
+		
+		
+		boolean hasErrorTestFail = false;
+		boolean getReturnValueTestFail = false;
+		boolean getErrorTestFail = false;
+		
+		
+		ReturnObject testObject = new ReturnObjectImpl(object, error);
+		
+		//tests that the getError message will match to the intended message
+		if (error != testObject.getError()){
+			getErrorTestFail = true;
+		}
+
+		//tests whether the hasError is returning correctly
+		if (error != ErrorMessage.NO_ERROR){
+			if (testObject.hasError() != false){
+				hasErrorTestFail = true;
+			}
+		}else{
+			if (testObject.hasError() != true){
+				hasErrorTestFail = true;
+			}
+		}
+		
+		//tests that the return object is returning correctly
+		if (error == ErrorMessage.NO_ERROR){
+			if (!testObject.getReturnValue().equals(object)){
+				getReturnValueTestFail = true;
+			}else{
+				if (testObject.getReturnValue() != null){
+					getReturnValueTestFail = true;
+				}
+			}
+		}
+		
+		//Prints messages if necessary
+		if (getErrorTestFail || hasErrorTestFail || getReturnValueTestFail){
+			System.out.println(message);
+			//TODO - Add in messages for specific tests
+		}
+		
 		
 	}
 	
