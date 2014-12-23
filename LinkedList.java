be14a54f6abc68eed4be95de693f23fd4214b68c
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2014-12-23 15:26:23 +0000

    Basic LinkedList Wrapper class & Node
    Node has no functionality as yet
    Wrapper class will return if head is null otherwise points at the node
    This is not tested.

---
+++ LinkedList.java
@@ -0,0 +1,63 @@
+
+public class LinkedList implements List {
+
+	LinkedListNode head;
+	
+	public LinkedList(){
+		head = null;
+	}
+	
+	
+	@Override
+	public boolean isEmpty() {
+		if (head == null) 
+			return false;
+		else
+			return true;
+	}
+
+	@Override
+	public int size() {
+		if (head == null)
+			return 0;
+		else 
+			return head.size();
+	}
+
+	@Override
+	public ReturnObject get(int index) {
+		if (head == null){
+			return new ReturnObjectImpl(null, ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		}else{
+			return head.get(index);
+		}
+	}
+
+	@Override
+	public ReturnObject remove(int index) {
+		if (head == null){
+			return new ReturnObjectImpl(null, ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		}else{
+			return head.remove(index);
+		}
+	}
+
+	@Override
+	public ReturnObject add(int index, Object item) {
+		if (head == null){
+			return new ReturnObjectImpl(null, ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		}else{
+			return head.add(index, item);
+		}
+	}
+
+	@Override
+	public ReturnObject add(Object item) {
+		if (head == null){
+			head = new LinkedListNode(0, item);
+			return new ReturnObjectImpl(null, ErrorMessage.NO_ERROR);
+		}else{
+			return head.add(item);
+		}
+	}
+}
---
+++ LinkedListNode.java
@@ -0,0 +1,50 @@
+
+public class LinkedListNode {
+
+	Object value;
+	int position;
+	
+	public LinkedListNode(int position, Object value){
+		this.value = value;
+		this.position = position;
+	}
+	
+	
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public ReturnObject get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ReturnObject remove(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ReturnObject add(int index, Object item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ReturnObject add(Object item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private void shuffleRight(){
+		
+	}
+	
+	private void shuffleLeft(){
+		
+	}
+}
